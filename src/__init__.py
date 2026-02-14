31c4cf7ed19b8ae50158fc54cfea5d8a22852e9b
Евгений Коваль <evgeniykoval833@mail.ru>
2026-02-14 14:35:44 +0300

    correct code1

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,9 @@
-from src.masks import get_mask_card_number, get_mask_account
-from src.widget import mask_account_card, get_date
+from src.masks import get_mask_account, get_mask_card_number
+from src.widget import get_date, mask_account_card
 
 __all__ = [
     'get_mask_card_number',
     'get_mask_account',
     'mask_account_card',
-    'get_date '
+    'get_date'
 ]
