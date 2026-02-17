a61c771a21db2e8c05f1c0b6cc90be8228fc9035
Евгений Коваль <evgeniykoval833@mail.ru>
2026-02-17 18:01:01 +0300

    new_def_sort

---
+++ src/processing.py
@@ -0,0 +1,52 @@
+def filter_by_state(transactions, state='EXCUTED'):
+    return [item for item in transactions if item.get('state') == state]
+"""
+Фильтрует список словарей по значению ключа 'state'.
+"""
+
+# Исходные данные
+data = [
+    {'id': 414288290, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+]
+
+executed_transactions = filter_by_state(data)
+print(executed_transactions)
+# Вывод: [{'id': 414288290, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+#         {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
+
+# Фильтрация по state='CANCELED'
+canceled_transactions = filter_by_state(data, 'CANCELED')
+print(canceled_transactions)
+
+def sort_by_date(transactions, reverse=True):
+    """
+    Сортирует список словарей по дате.
+    """
+    return sorted(transactions, key=lambda x: x ['date'], reverse=reverse)
+
+# Исходные данные
+data = [
+    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+]
+
+# Сортировка по убыванию (по умолчанию) - сначала новые
+sorted_desc = sort_by_date(data)
+print(sorted_desc)
+# Вывод:
+# [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+#  {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+#  {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+#  {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
+
+# Сортировка по возрастанию - сначала старые
+sorted_asc = sort_by_date(data, reverse=False)
+print(sorted_asc)
+
+# Проверка, что исходный список не изменился
+print("Исходный список:", data)
